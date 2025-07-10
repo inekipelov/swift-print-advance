19004a6061eb781ea3b5c4c190f4d93166999436
inekipelov <nekipelov.roma@gmail.com>
2025-07-10 14:45:01 +0300

    refactor: implement asynchronous printing in ConsolePrintOutput

--- Sources/PrintOutput/ConsolePrintOutput.swift
+++ Sources/PrintOutput/ConsolePrintOutput.swift
@@ -2,10 +2,19 @@
 //  ConsolePrintOutput.swift
 //
 
+import Dispatch
+
 typealias ConsolePrint = ConsolePrintOutput
+
+private let consolePrintSerialQueue = DispatchQueue(
+    label: "com.swift-print-advance.ConsolePrintOutput",
+    qos: .utility
+)
 
 public struct ConsolePrintOutput: PrintOutput {
     public mutating func write(_ string: String) {
-        Swift.print(string, terminator: "")
+        consolePrintSerialQueue.async {
+            Swift.print(string, terminator: "")
+        }
     }
 }
